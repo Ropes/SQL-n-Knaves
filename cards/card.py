f0fa456d0ae0a9c155737906b3eab20ceb2955bf
Josh Roppo <joshroppo@gmail.com>
2014-06-06 11:21:45 -0700

    Added Card class documentation
    Now that the class is mostly solidified, added docstring to the class to
    explain args and attributes.

--- cards/card.py
+++ cards/card.py
@@ -10,7 +10,16 @@ suits = OrderedDict({'Spades': ranks, 'Diamonds': ranks, 'Hearts': ranks,\
         'Clubs':ranks, 'Wild': wild})
 
 class Card(object):
-    
+    '''Card object represents a single card from the standard playing card
+    deck.  Contains attributes of Suit and Rank which are enforced to be
+    proper strings.  Equivalence of two cards will asser that both the suit
+    and rank attributes are equal.
+
+    Args and Attributes:
+        suit (unicode): Suit of the card. eg: 'Spades'
+        rank (unicode): Rank of the card. eg: 'Ace', '2', or 'King'
+    '''
+
     def __init__(self, suit, rank):
         try:
             assert suit in suits
