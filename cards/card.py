9fe4cc214aa5707c4c84438af67afa69850d2d65
Josh Roppo <joshroppo@gmail.com>
2014-06-06 10:31:56 -0700

    Card equality comparison and enforcing valid init
    Two cards can now be compared for equivalence based on Suit and Rank.
    Added Suit and Rank definitiions which will need to be improved but for
    now basically enforces Cards cannot have random strings as attributes.

--- cards/card.py
+++ cards/card.py
@@ -1,11 +1,28 @@
 from __future__ import print_function, unicode_literals
 
+suits = {'Spades', 'Diamonds', 'Hearts', 'Clubs', 'Wild'}
+ranks = {'Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10',\
+        'Jack', 'Queen', 'King', 'Joker'}
 
 class Card(object):
     
     def __init__(self, suit, rank):
-        self.suit = suit
-        self.rank = rank
+        try:
+            assert suit in suits 
+            self.suit = suit
+        except:
+            raise ValueError, 'Suit given not an acceptable value.'
+
+        try:
+            assert rank in ranks
+            self.rank = rank
+        except:
+            raise ValueError, 'Rank given not in acceptable ranks.'
 
     def __unicode__(self):
         return '{rank} of {suit}'.format(rank=self.rank, suit=self.suit)
+    
+    def __eq__(self, other):
+        return True if self.suit == other.suit and \
+                self.rank == other.rank else False
+
--- tests/test_card.py
+++ tests/test_card.py
@@ -8,9 +8,25 @@ class TestCards(unittest.TestCase):
     
     def test_init(self):
         suit = 'Spades'
-        
-        c = Card('Spades', 'Ace')
+        rank = 'Ace' 
+        c = Card(suit, rank)
 
-        self.assertEqual(c.suit, 'Spades')
-        self.assertEqual(c.rank, 'Ace')
+        self.assertEqual(c.suit, suit)
+        self.assertEqual(c.rank, rank)
         self.assertEqual(unicode(c), 'Ace of Spades')
+
+    def test_incorrect_init(self):
+
+        self.assertRaises(ValueError, Card, 'Batman', 'Joker')
+        #TODO: The following should be an error
+        #self.assertRaises(ValueError, Card, 'Spades', 'Joker')
+        self.assertRaises(ValueError, Card, 'Spades', 'Five')
+
+    def test_equality(self):
+        x = Card('Spades', 'Ace')
+        y = Card('Diamonds', 'King')
+        z = Card('Spades', 'Ace')
+
+        self.assertNotEqual(x,y)
+        self.assertEqual(x,z)
+        
